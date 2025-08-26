2553411df01c186be1d06fa7f19e67449170675c
mohamed osama <mohamed.osama1705@gmail.com>
2025-08-26 06:29:28 +0300

    deleted unnessary files

--- datasets/loan/archive/dashboard2.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.write("This is the second dashboard page.")
-
-st.sidebar.subheader("Models")
-models=["Model A", "Model B", "Model C"]
-st.sidebar.selectbox(
-    "Model Selection",
-    options=models)
